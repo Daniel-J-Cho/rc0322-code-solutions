370e7c5f0306bf6aebb4306e354383b9bb1d6be7
Daniel Cho <danielcho07@gmail.com>
2022-03-21 19:25:16 +0000

    Completed javascript-operators-and-expressions exercise.

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,42 @@
+var width = 50;
+var height = 35;
+
+var area = width * height;
+console.log('area: ', area);
+console.log('typeof area: ', typeof area);
+
+var bill = 1000;
+var payment = 5;
+
+var change = bill - payment;
+console.log('change: ', change);
+console.log('typeof change: ', typeof change);
+
+var quizzes = 100;
+var midterm = 100;
+var final = 100;
+
+var grade = (quizzes + midterm + final) / 3;
+console.log('grade is: ', grade);
+console.log('typeof grade: ', typeof grade);
+
+var firstName = 'Daniel ';
+var lastName = 'Cho';
+var fullName = firstName + lastName;
+console.log('Full name: ', fullName);
+console.log('typeof fullName: ', typeof fullName);
+
+var pH = 1;
+var isAcidic = pH < 7;
+console.log('is it acidic?: ', isAcidic);
+console.log('typeof isAcidic: ', typeof isAcidic);
+
+var headCount = 9;
+var isSparta = headCount === 300;
+console.log('is headCount equal to isSparta?: ', isSparta);
+console.log('typeof isSparta: ', typeof isSparta);
+
+var motto = fullName;
+motto += ' is the GOAT';
+console.log('value of motto: ', motto);
+console.log('typeof motto: ', typeof motto);
