7ac6d5c644315f40c8450e396197be861c2dbfac
Daniel Cho <danielcho07@gmail.com>
2022-03-26 01:20:57 +0000

    Completed object-manipulation-1 exercise.

---
+++ object-manipulation-1/src/get-value.js
@@ -0,0 +1,8 @@
+/* exported getValue */
+
+function getValue(object, key) {
+  return object[key];
+}
+
+// Access the key in object and return
+// the value with the associated key.
---
+++ object-manipulation-1/src/set-value.js
@@ -0,0 +1,8 @@
+/* exported setValue */
+
+function setValue(object, key, value) {
+  object[key] = value;
+}
+
+// Assign value to the key of the object
+// using bracket notation.
