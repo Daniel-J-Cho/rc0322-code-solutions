b2e6a9dfbf752a4638eb4da974fda9f389e85105
Daniel Cho <danielcho07@gmail.com>
2022-03-26 23:40:27 +0000

    Completed array-manipulation-2 exercise.

--- array-manipulation-2/src/drop-right.js
+++ array-manipulation-2/src/drop-right.js
@@ -1,1 +1,13 @@
 /* exported dropRight */
+
+function dropRight(array, count) {
+  var newArray = [];
+  for (let i = 0; i < array.length - count; i++) {
+    if (array[i] !== undefined) {
+      newArray.push(array[i]);
+    } else if (array === []) {
+      newArray = [];
+    }
+  }
+  return newArray;
+}
--- array-manipulation-2/src/take-right.js
+++ array-manipulation-2/src/take-right.js
@@ -2,13 +2,15 @@
 
 function takeRight(array, count) {
   var newArray = [];
-  for (let i = -count; i >= 0; i--) {
-    if (array[i] !== undefined) {
-      newArray.push(array[i]);
-    } else if (count > array.length) {
+  for (let i = array.length - count; i < array.length; i++) {
+    if (count > array.length) {
       newArray = array;
     } else if (array === []) {
       newArray = [];
+    } else if (array[i] !== undefined) {
+      newArray.push(array[i]);
+    } else if (array[i] === undefined) {
+      newArray.push(undefined);
     }
   }
   return newArray;
@@ -17,5 +19,7 @@ function takeRight(array, count) {
 // Create a new empty array and assign to a new variable.
 // Loop through the array passed in and push the last
 // 'count' indexes of the array onto the newly created
-// empty array.  Do this using the 'at()' method passing in
-// the negative version of the 'count' argument.
+// empty array.  Do this using an if, else if statement.
+// First specify the conditions where if count is larger
+// than the length of the array and if array is an empty
+// array.
