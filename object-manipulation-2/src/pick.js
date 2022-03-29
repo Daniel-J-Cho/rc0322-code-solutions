fd562f34c19aac8b4023d1ba62078b3a842a73a1
Daniel Cho <danielcho07@gmail.com>
2022-03-29 17:17:23 +0000

    Completed object-manipulation-2 exercise.

--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -3,8 +3,8 @@
 function pick(source, keys) {
   var newObject = {};
   for (let i = 0; i < keys.length; i++) {
-    if ((keys[i] === source[keys[i]]) && (source[keys[i]] !== undefined)) {
-      newObject = source[keys[i]];
+    if (source[keys[i]] !== undefined) {
+      newObject[keys[i]] = source[keys[i]];
     } else {
       continue;
     }
@@ -15,8 +15,12 @@ function pick(source, keys) {
 // Create empty object and assign it to
 // a new variable.
 // Loop through indexes of 'keys' array.
-// Check to see if index 'i' of 'keys'
-// is a property of 'source' object.
-// If it is, add it to new object.
+// Pass in value at keys[i] as a property
+// of 'source' object; and use strictly
+// unequal operator against 'undefined'
+// to make sure value at source[keys[i]]
+// is not undefined.
+// If it isn't, add it to new object at
+// newobject[keys[i]].
 // Else, continue to next index.
 // Return new object.
