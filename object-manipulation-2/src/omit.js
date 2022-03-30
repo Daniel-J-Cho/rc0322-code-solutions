8d172e66a27e1772a609528f8c332e67531acefd
Daniel Cho <danielcho07@gmail.com>
2022-03-30 16:43:55 +0000

    Completed object-manipulation-2 exercise.

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,1 +1,20 @@
 /* exported omit */
+
+function omit(source, keys) {
+  var newObj = {};
+  for (const key in source) {
+    if (keys.includes(key)) {
+      return false;
+    } else {
+      newObj[key] = source[key];
+    }
+  }
+  return newObj;
+}
+
+// Create an empty object and assign it
+// to a new variable.
+// Loop through 'keys' array. If keys[i]
+// is a property of 'source', return false.
+// Else, add property of 'source' into
+// new empty object. Return new object.
