8aa233d3180775383c7d55396b18af571d9be6fa
Daniel Cho <danielcho07@gmail.com>
2022-03-30 03:54:05 +0000

    Completed javascript-typing-tutor exercise.

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,3 +1,20 @@
 var $spanElements = document.querySelectorAll('span');
+var counter = 0;
 
-$spanElements.addEventListnere('keydown');
+function typeMatch(event) {
+  var keyCharacter = event.key;
+  if (counter === $spanElements.length || counter > $spanElements.length) {
+    return false;
+  } else if (keyCharacter === $spanElements[counter].textContent) {
+    $spanElements[counter].className = 'green-char'; // make this character green;
+    counter++;// increment
+    if (counter === $spanElements.length) {
+      return false;
+    }
+    $spanElements[counter].className = 'underline';// make this character with an underline
+  } else if (keyCharacter !== $spanElements[counter].textContent) { // else statement
+    $spanElements[counter].className = 'red-char';// set character to red
+  }
+}
+
+document.addEventListener('keydown', typeMatch);
