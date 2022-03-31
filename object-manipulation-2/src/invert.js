d94778929883570c16a6d50e027ca1577d663ba8
Daniel Cho <danielcho07@gmail.com>
2022-03-31 22:46:07 +0000

    Completed object-manipulation-2 exercise.

--- object-manipulation-2/src/invert.js
+++ object-manipulation-2/src/invert.js
@@ -1,1 +1,12 @@
 /* exported invert */
+
+function invert(source) {
+  var newObj = {};
+  for (var key in source) {
+    newObj[source[key]] = key;
+  }
+  return newObj;
+}
+
+// swapping keys and values of objects
+//
