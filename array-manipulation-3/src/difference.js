3feedcc513a31bf4a7daaedfe212272e7f355c33
Daniel Cho <danielcho07@gmail.com>
2022-04-02 20:08:43 +0000

    Completed array-manipulation-3 exercise.

--- array-manipulation-3/src/difference.js
+++ array-manipulation-3/src/difference.js
@@ -1,1 +1,21 @@
 /* exported difference */
+
+function difference(first, second) {
+  var newArr = [];
+  for (let i = 0; i < first.length; i++) {
+    if (second.includes(first[i])) {
+      continue;
+    } else {
+      newArr.push(first[i]);
+    }
+  }
+  for (let j = 0; j < second.length; j++) {
+    if (first.includes(second[j])) {
+      continue;
+    } else {
+      newArr.push(second[j]);
+    }
+  }
+
+  return newArr;
+}
--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -2,6 +2,20 @@
 
 function zip(first, second) {
 
+  // var newArr = [];
+  // if (first.length < second.length) {
+  //   for (let i = 0; i < first.length; i++) {
+
+  //   }
+  // } else if (first.length > second.length) {
+  //   for (let j = 0; j < second.length; j++) {
+  //     second.push(first[j]);
+  //   }
+  // } else if (first.length === second.length) {
+  //   for (let k = 0; k < first.length; k++) {
+  //     first.push(second[k]);
+  //   }
+  // }
 }
 
 // Create new array and assign it to a
