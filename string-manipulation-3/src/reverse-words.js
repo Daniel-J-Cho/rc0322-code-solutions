69d877bf88f549ca887eb3b4a512ba80a02a9a78
Daniel Cho <danielcho07@gmail.com>
2022-04-03 21:21:11 +0000

    Completed string-manipulation-3 exercise.

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -5,9 +5,13 @@ function reverseWords(string) {
   var spltStr = string.split(' ');
   var revSpltStr = [];
   var secondSplt = [];
+  var revWord = [];
+  var joinWrd = [];
   for (let i = 0; i < spltStr.length; i++) {
-    secondSplt = spltStr[i].split('').reverse().join('');
-    revSpltStr.push(secondSplt);
+    secondSplt = spltStr[i].split('');
+    revWord = secondSplt.reverse();
+    joinWrd = revWord.join('');
+    revSpltStr.push(joinWrd);
   }
   newStr = revSpltStr.join(' ');
   return newStr;
