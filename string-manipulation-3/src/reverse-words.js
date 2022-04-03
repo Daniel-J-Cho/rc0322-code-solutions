75eb73b31869439423f0c030252a246f059cdbb7
Daniel Cho <danielcho07@gmail.com>
2022-04-03 21:08:37 +0000

    Completed string-manipulation-3 exercise.

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,4 +1,14 @@
 /* exported reverseWords */
 
-// Use '.split(' ')' to break up words
-// in string
+function reverseWords(string) {
+  var newStr = '';
+  var spltStr = string.split(' ');
+  var revSpltStr = [];
+  var secondSplt = [];
+  for (let i = 0; i < spltStr.length; i++) {
+    secondSplt = spltStr[i].split('').reverse().join('');
+    revSpltStr.push(secondSplt);
+  }
+  newStr = revSpltStr.join(' ');
+  return newStr;
+}
