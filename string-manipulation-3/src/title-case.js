edcc42df1e2e795fe59397959fb3e5dae034ffbc
Daniel Cho <danielcho07@gmail.com>
2022-04-13 06:28:14 +0000

    Completed string-manipulation-3 exercise.

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,1 +1,10 @@
 /* exported titleCase */
+
+function titleCase(title) {
+  // var spltStr = title.toLowerCase().split(' ');
+  // for (var i = 0; i < spltStr.length; i++) {
+  //   for (var j = 0; j < spltStr[i][1]; j++) {
+  //     var firstLetter = spltStr[i][j].toUpperCase();
+  //   }
+  // }
+}
