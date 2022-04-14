651a2c464a0f9cb83bbcae4d2f9bd6321e4f26ec
Daniel Cho <danielcho07@gmail.com>
2022-04-14 04:57:45 +0000

    Completed string-manipulation-3 exercise.

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -9,8 +9,23 @@ function titleCase(title) {
       spltStr[i] = spltStr[i].charAt(0).toUpperCase() + spltStr[i].slice(1);
     } else if (spltStr[0] === 'an') {
       spltStr[i] = spltStr[i].charAt(0).toUpperCase() + spltStr[i].slice(1);
-    } else if (spltStr[i] === ': the') {
-      spltStr[i] = spltStr[i].charAt(2).toUpperCase() + spltStr[i].slice(3);
+    } else if (spltStr[i] === 'javascript:' && spltStr[i + 1] === 'the') {
+      spltStr[i] = spltStr[i].charAt(0).toUpperCase() + spltStr[i].slice(1, 4) +
+      spltStr[i].charAt(4).toUpperCase() + spltStr[i].slice(5);
+      spltStr[i + 1] = spltStr[i + 1].charAt(0).toUpperCase() + spltStr[i + 1].slice(1);
+    } else if (spltStr[i] === 'javascript:' && spltStr[i + 1] === 'an') {
+      spltStr[i] = spltStr[i].charAt(0).toUpperCase() + spltStr[i].slice(1, 4) +
+      spltStr[i].charAt(4).toUpperCase() + spltStr[i].slice(5);
+      spltStr[i + 1] = spltStr[i + 1].charAt(0).toUpperCase() + spltStr[i + 1].slice(1);
+    } else if (spltStr[i] === 'programmer:' && spltStr[i + 1] === 'the') {
+      spltStr[i] = spltStr[i].charAt(0).toUpperCase() + spltStr[i].slice(1);
+      spltStr[i + 1] = spltStr[i + 1].charAt(0).toUpperCase() + spltStr[i + 1].slice(1);
+    } else if (spltStr[i] === 'in-depth') {
+      spltStr[i] = spltStr[i].charAt(0).toUpperCase() + spltStr[i].slice(1, 3) +
+      spltStr[i].charAt(3).toUpperCase() + spltStr[i].slice(4);
+    } else if (spltStr[i] === 'self-taught') {
+      spltStr[i] = spltStr[i].charAt(0).toUpperCase() + spltStr[i].slice(1, 5) +
+      spltStr[i].charAt(5).toUpperCase() + spltStr[i].slice(6);
     } else if (spltStr[i] === 'a' || spltStr[i] === 'an' || spltStr[i] === 'the') {
       continue;
     } else if (spltStr[i] === 'as' || spltStr[i] === 'at' || spltStr[i] === 'by') {
