4bdd17374320656fb225972ef218e425c80b0f4d
Daniel Cho <danielcho07@gmail.com>
2022-05-24 05:30:29 +0000

    Completed react-carousel-component exercise.

--- react-carousel-component/src/index.jsx
+++ react-carousel-component/src/index.jsx
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client';
 import Carousel from './carousel.jsx';
 
 const images = [
-  { id: 1, imgUrl: './images/001.png' },
-  { id: 2, imgUrl: './images/004.png' },
-  { id: 3, imgUrl: './images/007.png' },
-  { id: 4, imgUrl: './images/025.png' },
-  { id: 5, imgUrl: './images/039.png' }
+  { imgUrl: './images/001.png' },
+  { imgUrl: './images/004.png' },
+  { imgUrl: './images/007.png' },
+  { imgUrl: './images/025.png' },
+  { imgUrl: './images/039.png' }
 ];
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
