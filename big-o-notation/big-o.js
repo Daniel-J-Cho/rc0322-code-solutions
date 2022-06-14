0aaf83b22375a35cca2e44efb56fdd09598dea34
Daniel Cho <danielcho07@gmail.com>
2022-06-14 00:01:23 +0000

    Completed big-o-notation exercise.

--- big-o-notation/big-o.js
+++ big-o-notation/big-o.js
@@ -9,12 +9,12 @@ function uniqueLinear(words) {
     i++                             // 2 * n = O(2n) ~= 0(n)
   ) {
     const word = words[i];          // 2 * n = O(2n) ~= 0(n)
-    if (!seen[word]) {              // 3 * n = O(3n) ~= 0(n)
-      seen[word] = true;
-      unique[unique.length] = word; // 3 * n = O(3n) ~= 0(n)
+    if (!seen[word]) {              // 2 * n = O(2n) ~= 0(n)
+      seen[word] = true;            // 1 * n = 0(n)
+      unique[unique.length] = word; // 2 * n = O(2n) ~= 0(n)
     }
   }
-  return unique;
+  return unique;                    // 1 * 1 = 0(1)
 } // Big O Notation for uniqueLinear: O(n)
 
 function uniqueQuadratic(words) {
@@ -25,20 +25,20 @@ function uniqueQuadratic(words) {
     i++                             // 2 * n = O(2n) ~= 0(n)
   ) {
     const word = words[i];          // 2 * n = O(2n) ~= 0(n)
-    let isUnique = true;            // 2 * 1 = O(2)
+    let isUnique = true;            // 1 * n = O(n)
     for (
-      let c = 0;                    // 1 * 1 = O(1)
-      c < i;                        // 1 * n = O(n)
-      c++                           // 2 * n = O(2n) ~= 0(n)
+      let c = 0;                    // 1 * n = O(n)
+      c < i;                        // 1 * n^2 = O(n^2)
+      c++                           // 2 * n^2 = O(2(n^2)) ~= 0(n^2)
     ) {
-      const comparing = words[c];   // 2 * n = O(2n) ~= 0(n)
-      if (comparing === word) {     // 1 * n = O(n)
-        isUnique = false;           // 1 * 1 = O(1)
+      const comparing = words[c];   // 2 * n^2 = O(2(n^2)) ~= 0(n^2)
+      if (comparing === word) {     // 1 * n^2 = O(n^2)
+        isUnique = false;           // 1 * n^2 = O(n^2)
       }
     }
-    if (isUnique) {                 // 1 * 1 = O(1)
-      unique[unique.length] = word; // 3 * n = O(3n) ~= 0(n)
+    if (isUnique) {                 // 1 * n = O(n)
+      unique[unique.length] = word; // 2 * n = O(2n) ~= 0(n)
     }
   }
   return unique;                    // 1 * 1 = O(1)
-} // Big O Notation for uniqueQuadratic: O(2)
+} // Big O Notation for uniqueQuadratic: O(n^2)
